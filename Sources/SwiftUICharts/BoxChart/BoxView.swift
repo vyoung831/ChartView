c8abc482d4c6f8030ca0d971d7f05d3f136c76a9
Vincent Young <v.young831@yahoo.com>
2021-07-13 14:34:35 -0400

    Add DragGesture to BoxView to magnify boxes

--- Sources/SwiftUICharts/BoxChart/BoxView.swift
+++ Sources/SwiftUICharts/BoxChart/BoxView.swift
@@ -20,17 +20,19 @@ public struct BoxView: View {
     @State var currentTouchedIndex: (Int, Int) = (-1,-1)
     
     /*
-     Presentation constants. These must be defined so that the boxes that DragGestures intersect with can be identified
+     Presentation constants. These must be defined so that DragGestures can identified and magnify boxes.
      - intraSectionSpacing: Spacing between each section's title text and its chart of boxes.
-     - interSectionSpacing: VStack spacing between each section
-     - rowSpacing: Spacing between each row of boxes
+     - interSectionSpacing: VStack spacing between each section.
+     - boxSpacing: Horizontal spacing between boxes in the same row.
+     - rowSpacing: Vertical spacing between each row of boxes.
      */
     let mainVStackSpacing: CGFloat = 25
     let sectionTitleHeight: CGFloat = 20
     let interSectionSpacing: CGFloat = 20
     let intraSectionSpacing: CGFloat = 10
     let boxHeight: CGFloat = 12
-    let rowSpacing: CGFloat = 8
+    let boxSpacing: CGFloat = 8
+    let rowSpacing: CGFloat = 10
     
     public init(style: ChartStyle, sections: [(String, BoxChartData)], title: String?, subtext: String?, boxesPerRow: Int) {
         self.style = style
@@ -47,6 +49,73 @@ public struct BoxView: View {
             gridItems.append(GridItem())
         }
         return gridItems
+    }
+    
+    /**
+     Finds the indices of the section (in `sections`) and box (within the section's data) that a gesture location intersects with.
+     `touchLocation`'s y-value must intersect with a section's LazyVGrid for a box to be identified for magnifying. The box closest to `touchLocation`'s x-value and y-values is then selected.
+     If `touchLocation`'s y-value falls between sections, in a section's labels/spacing, or above/under the VStack of sections, no box is magnified.
+     - parameter touchLocation: Location of the touch in the VStack of sections.
+     - parameter totalWidth: The total width of the VStack of sections.
+     - returns: Tuple containing the indicies of the section and box that touchLocation is closest to in the VStack fo sections
+     */
+    private func boxToMagnify(touchLocation: CGPoint, totalWidth: CGFloat) -> (Int, Int) {
+        
+        // y-value is above the VStack of sections. Return invalid indices.
+        if touchLocation.y < 0 {
+            return (-1,-1)
+        }
+        
+        // Get row counts and heights of each section
+        let sectionData: [(rows: Int, height: CGFloat)] = sections.map({
+            let rowCount = ceil( Double($0.data.points.count)/Double(boxesPerRow) )
+            return (rows: Int(rowCount),
+                    height: (CGFloat(rowCount) * boxHeight) + (CGFloat(rowCount - 1) * rowSpacing))
+        })
+        
+        // Search for which section touchLocation is in.
+        var searchedHeight: CGFloat = 0
+        let deadzoneHeight: CGFloat = sectionTitleHeight + intraSectionSpacing
+        for sectionIdx in 0 ..< sectionData.count {
+            if touchLocation.y < searchedHeight + deadzoneHeight + sectionData[sectionIdx].height {
+                
+                // If touchLocation's y-value falls in the dead zone (the section's title or the spacing between the section and the LazyVGrid), return invalid indices.
+                if touchLocation.y < searchedHeight + deadzoneHeight {
+                    return (-1, -1)
+                } else {
+
+                    // Iterate through the section's rows and find the row that `touchLocation` is vertically closest to.
+                    var endOfCurrentRow: CGFloat = boxHeight + (rowSpacing/2)
+                    var touchedRow: Int = 0
+                    for rowIdx in 0 ..< sectionData[sectionIdx].rows {
+                        if touchLocation.y - (searchedHeight + deadzoneHeight) > endOfCurrentRow {
+                            endOfCurrentRow = endOfCurrentRow + boxHeight + rowSpacing
+                        } else {
+                            touchedRow = rowIdx; break
+                        }
+                    }
+                    
+                    // Find the box that's horizontally closest to `touchLocation`
+                    let boxWidth = (totalWidth - (CGFloat(boxesPerRow - 1) * boxSpacing)) / CGFloat(boxesPerRow)
+                    for boxIdx in 0 ..< boxesPerRow {
+                        let totalWidth = (CGFloat(boxIdx + 1) * boxWidth)
+                        let totalSpacing = (CGFloat(boxIdx) * boxSpacing) + (boxSpacing)/2
+                        if touchLocation.x < totalWidth + totalSpacing {
+                            return (sectionIdx, (touchedRow * boxesPerRow) + boxIdx)
+                        }
+                    }
+                    return (sectionIdx, (touchedRow * boxesPerRow) + boxesPerRow - 1)
+                    
+                }
+                
+            } else {
+                searchedHeight = searchedHeight + deadzoneHeight + sectionData[sectionIdx].height + interSectionSpacing
+            }
+        }
+        
+        // y-value is under VStack of sections. Return invalid indices.
+        return (-1, -1)
+        
     }
     
     public var body: some View {
@@ -68,10 +137,12 @@ public struct BoxView: View {
                         .foregroundColor(self.style.textColor)
                 }
                 
+                // VStack of sections
                 VStack(spacing: self.interSectionSpacing) {
                     
                     ForEach(0 ..< sections.count, id: \.self) { sectionIdx in
                         
+                        // Section consisting of title and box chart
                         VStack(alignment: .leading, spacing: self.intraSectionSpacing) {
                             
                             Text(sections[sectionIdx].sectionTitle)
@@ -90,11 +161,6 @@ public struct BoxView: View {
                                             .onTapGesture {
                                                 self.currentTouchedIndex = (sectionIdx, idx)
                                             }
-                                            .gesture(
-                                                DragGesture()
-                                                    .onChanged({ value in
-                                                        self.currentTouchedIndex = (sectionIdx, idx)
-                                                    }))
                                             .scaleEffect(self.currentTouchedIndex == (sectionIdx, idx) ? 1.25 : 1)
                                             .animation(Animation.spring())
                                     }
@@ -106,6 +172,11 @@ public struct BoxView: View {
                     }
                     
                 }
+                .gesture(
+                    DragGesture()
+                        .onChanged({ dragPoint in
+                            self.currentTouchedIndex = self.boxToMagnify(touchLocation: dragPoint.location, totalWidth: gr.size.width)
+                        }))
                 
             }
             
