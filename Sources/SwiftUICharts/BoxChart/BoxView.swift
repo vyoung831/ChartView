8be7b37093592f8ced134d0ee762c1512c2e5de7
Vincent Young <v.young831@yahoo.com>
2021-07-12 20:18:27 -0400

    Define presentation constants in BoxView and update Views to use them

--- Sources/SwiftUICharts/BoxChart/BoxView.swift
+++ Sources/SwiftUICharts/BoxChart/BoxView.swift
@@ -17,7 +17,20 @@ public struct BoxView: View {
     var title: String?
     var subtext: String?
     var boxesPerRow: Int
-    @State var currentTouchedIndex: (Int, Int) = (0,0)
+    @State var currentTouchedIndex: (Int, Int) = (-1,-1)
+    
+    /*
+     Presentation constants. These must be defined so that the boxes that DragGestures intersect with can be identified
+     - intraSectionSpacing: Spacing between each section's title text and its chart of boxes.
+     - interSectionSpacing: VStack spacing between each section
+     - rowSpacing: Spacing between each row of boxes
+     */
+    let mainVStackSpacing: CGFloat = 25
+    let sectionTitleHeight: CGFloat = 20
+    let interSectionSpacing: CGFloat = 20
+    let intraSectionSpacing: CGFloat = 10
+    let boxHeight: CGFloat = 12
+    let rowSpacing: CGFloat = 8
     
     public init(style: ChartStyle, sections: [(String, BoxChartData)], title: String?, subtext: String?, boxesPerRow: Int) {
         self.style = style
@@ -38,48 +51,58 @@ public struct BoxView: View {
     
     public var body: some View {
         
-        VStack(alignment: .leading, spacing: 25) {
+        GeometryReader { gr in
             
-            if let titleString = self.title {
-                Text(titleString)
-                    .bold()
-                    .font(.title)
-                    .foregroundColor(self.style.textColor)
-            }
-            
-            if let subtextString = self.subtext {
-                Text(subtextString)
-                    .font(.callout)
-                    .foregroundColor(self.style.textColor)
-            }
-            
-            ForEach(0 ..< sections.count, id: \.self) { sectionIdx in
+            VStack(alignment: .leading, spacing: self.mainVStackSpacing) {
                 
-                VStack(alignment: .leading, spacing: 5) {
-                    
-                    Text(sections[sectionIdx].sectionTitle)
+                if let titleString = self.title {
+                    Text(titleString)
                         .bold()
+                        .font(.title)
+                        .foregroundColor(self.style.textColor)
+                }
+                
+                if let subtextString = self.subtext {
+                    Text(subtextString)
+                        .font(.callout)
+                        .foregroundColor(self.style.textColor)
+                }
+                
+                VStack(spacing: self.interSectionSpacing) {
                     
-                    if #available(iOS 14.0, *) {
+                    ForEach(0 ..< sections.count, id: \.self) { sectionIdx in
                         
-                        // onTapGesture is used before the gesture modifier w/DragGesture so that the DragGesture is delayed (https://www.hackingwithswift.com/forums/swiftui/a-guide-to-delaying-gestures-in-scrollview/6005)
-                        LazyVGrid(columns: vGridItems) {
-                            ForEach(0 ..< sections[sectionIdx].data.onlyPoints().count, id: \.self) { idx in
-                                Rectangle()
-                                    .foregroundColor(sections[sectionIdx].data.getColor(
-                                                        sections[sectionIdx].data.onlyPoints()[idx]))
-                                    .onTapGesture {
-                                        self.currentTouchedIndex = (sectionIdx, idx)
-                                    }
-                                    .gesture(
-                                        DragGesture()
-                                            .onChanged({ value in
+                        VStack(alignment: .leading, spacing: self.intraSectionSpacing) {
+                            
+                            Text(sections[sectionIdx].sectionTitle)
+                                .bold()
+                                .frame(height: self.sectionTitleHeight)
+                            
+                            if #available(iOS 14.0, *) {
+                                
+                                // onTapGesture is used before the gesture modifier w/DragGesture so that the DragGesture is delayed (https://www.hackingwithswift.com/forums/swiftui/a-guide-to-delaying-gestures-in-scrollview/6005)
+                                LazyVGrid(columns: vGridItems, spacing: self.rowSpacing) {
+                                    ForEach(0 ..< sections[sectionIdx].data.onlyPoints().count, id: \.self) { idx in
+                                        Rectangle()
+                                            .frame(height: self.boxHeight)
+                                            .foregroundColor(sections[sectionIdx].data.getColor(
+                                                                sections[sectionIdx].data.onlyPoints()[idx]))
+                                            .onTapGesture {
                                                 self.currentTouchedIndex = (sectionIdx, idx)
-                                            }))
-                                    .scaleEffect(self.currentTouchedIndex == (sectionIdx, idx) ? 1.25 : 1)
-                                    .animation(Animation.spring())
+                                            }
+                                            .gesture(
+                                                DragGesture()
+                                                    .onChanged({ value in
+                                                        self.currentTouchedIndex = (sectionIdx, idx)
+                                                    }))
+                                            .scaleEffect(self.currentTouchedIndex == (sectionIdx, idx) ? 1.25 : 1)
+                                            .animation(Animation.spring())
+                                    }
+                                }
                             }
+                            
                         }
+                        
                     }
                     
                 }
